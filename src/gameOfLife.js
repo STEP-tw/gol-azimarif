68588049a53efe229215a456391a318b6977ad73
Arif <44020819+azimarif@users.noreply.github.com>
2018-11-28 19:36:53 +0530

    Refactor isPositionExists to filter out invalid positions

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -1,10 +1,14 @@
 const { world } = require('./gameLibrary.js');
 
 const isPositionExists = function(currentAliveCell){
-  let { rowPosition, columnPosition, topLeft } = currentAliveCell;
+  let { rowPosition, columnPosition, topLeft, bottomRight } = currentAliveCell;
   let gridStartingRow = topLeft[0];
   let gridStartingColumn = topLeft[1];
-  return (rowPosition >= gridStartingRow && columnPosition >= gridStartingColumn);
+
+  let gridEndRow = bottomRight[0];
+  let gridEndColumn = bottomRight[1];
+  return (rowPosition >= gridStartingRow && columnPosition >= gridStartingColumn) && 
+    (rowPosition <= gridEndRow && columnPosition <= gridEndColumn);
 }
 
 const nextGeneration = function(currGeneration,bounds) {
@@ -13,7 +17,7 @@ const nextGeneration = function(currGeneration,bounds) {
   world.grid = world.generateGrid(size);
   
   currGeneration = currGeneration.filter((position)=> {
-    return isPositionExists({rowPosition: position[0], columnPosition: position[1], topLeft})
+    return isPositionExists({rowPosition: position[0], columnPosition: position[1], topLeft, bottomRight})
   });
 
   currGeneration.map((aliveCell)=> { world.initializeGrid( { latitude:aliveCell[0], longitude:aliveCell[1] })});
