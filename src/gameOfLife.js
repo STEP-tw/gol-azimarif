757d08988d6ee7fb14dc323d3a4852ac3cbe525c
Arif <44020819+azimarif@users.noreply.github.com>
2018-11-28 19:17:39 +0530

    Implement isPositionExists to filter valid positions

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -1,10 +1,21 @@
 const { world } = require('./gameLibrary.js');
 
+const isPositionExists = function(currentAliveCell){
+  let { rowPosition, columnPosition, topLeft } = currentAliveCell;
+  let gridStartingRow = topLeft[0];
+  let gridStartingColumn = topLeft[1];
+  return (rowPosition >= gridStartingRow && columnPosition >= gridStartingColumn);
+}
+
 const nextGeneration = function(currGeneration,bounds) {
   let  {topLeft,  bottomRight } = bounds; 
   let size = Math.max.apply(null,bottomRight) + 1;
   world.grid = world.generateGrid(size);
   
+  currGeneration = currGeneration.filter((position)=> {
+    return isPositionExists({rowPosition: position[0], columnPosition: position[1], topLeft})
+  });
+
   currGeneration.map((aliveCell)=> { world.initializeGrid( { latitude:aliveCell[0], longitude:aliveCell[1] })});
   world.updateGrid();
   let newGeneration = []; 
