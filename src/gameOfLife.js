feb336ea208ba9e04ad040167f72b09788db0fdd
Arif <44020819+azimarif@users.noreply.github.com>
2018-11-29 04:03:53 +0530

    Add getAliveCells to return all alive cell in list

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -11,27 +11,38 @@ const isPositionExists = function(currentAliveCell){
     (rowPosition <= gridEndRow && columnPosition <= gridEndColumn);
 }
 
+const getAliveCells = function(bounds){
+  let { topLeft, bottomRight } = bounds;
+  let newGeneration = [];
+  let rowIndex = generateIndex(topLeft[0], bottomRight[0]);
+  let columnIndex = generateIndex(topLeft[1], bottomRight[1]);
+
+  rowIndex.map((row)=>
+    columnIndex.map((col)=> {
+      world.grid[row][col] == 1 && newGeneration.push([row,col]);
+    })
+  );
+  return newGeneration;
+}
+
+const generateIndex = function(startIndex, endIndex){
+  let size = endIndex - startIndex + 1;
+  let index = startIndex;
+  return new Array(size).fill(0).map((element)=> index++ );
+}
+
 const nextGeneration = function(currGeneration,bounds) {
   let  {topLeft,  bottomRight } = bounds; 
   let size = Math.max.apply(null,bottomRight) + 1;
   world.grid = world.generateGrid(size);
-  
+
   currGeneration = currGeneration.filter((position)=> {
     return isPositionExists({rowPosition: position[0], columnPosition: position[1], topLeft, bottomRight})
   });
-
-  currGeneration.map((aliveCell)=> { world.initializeGrid( { latitude:aliveCell[0], longitude:aliveCell[1] })});
+  currGeneration.forEach((aliveCell)=> { world.initializeGrid( { latitude:aliveCell[0], longitude:aliveCell[1] })});
   world.updateGrid();
-  let newGeneration = []; 
 
-  world.grid.map((gridRow)=> 
-    gridRow.map((cell)=> {
-      return cell==1 && 
-         newGeneration.push([world.grid.indexOf(gridRow), gridRow.indexOf(cell)]);
-      
-    })
-  )
-  return newGeneration;
+  return getAliveCells(bounds);
 }
 
 module.exports = { nextGeneration };
