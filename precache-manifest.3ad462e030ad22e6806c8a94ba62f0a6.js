b02135b36fff059a475e26bc4c6bd36066155f3c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-03 05:28:06 +0000

    Deploy STEP-tw/gol-azimarif to github.com/STEP-tw/gol-azimarif.git:gh-pages

---
+++ precache-manifest.3ad462e030ad22e6806c8a94ba62f0a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d69ee5879994e98f6684",
+    "url": "/gol-azimarif/static/js/runtime~main.d69ee587.js"
+  },
+  {
+    "revision": "c3b4a5e9e133a84a2266",
+    "url": "/gol-azimarif/static/js/main.c3b4a5e9.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/gol-azimarif/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "c3b4a5e9e133a84a2266",
+    "url": "/gol-azimarif/static/css/main.8b3e9e37.chunk.css"
+  },
+  {
+    "revision": "c391620ded4317c05d029db38f019186",
+    "url": "/gol-azimarif/index.html"
+  }
+];
